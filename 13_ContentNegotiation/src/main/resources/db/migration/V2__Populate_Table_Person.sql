febf629301c6f1dfe2a77e56d8ae4c07a8f4c34c
Arthur de Menezes <arthurmenezes554@gmail.com>
2024-04-23 17:18:12 -0300

    Implementing Content Negotiation with JSON, XML and YAML

---
+++ 13_ContentNegotiation/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'South Korea','Kim','female','Minji'),
+    (2,'Australia','Hanni','female','Pham'),
+    (3,'Brasil','Arthur','male','João'),
+    (5,'South Korea - Seul','Lee','female','Hyein'),
+    (6,'South Korea - Seul','Danielle','female','Marsh');
+
+
